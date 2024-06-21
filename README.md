327b6371869c2de7800dcb91fbe2b8b3be7a4c6b
Orçun Çolak <orcun.colak@hazelcast.com>
2024-06-21 10:59:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Read Me
+
+The original idea is from  
+https://medium.com/@salmanynasim/docchat-interactive-chat-with-documents-using-spring-ai-openai-6e016f7c6455?source=explore---------2-98--------------------ae10fcfc_ead2_478f_a282_e7693318a200-------15
---
+++ src/main/java/com/colak/springtutorial/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.colak.springtutorial.config;
+
+
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AppConfig {
+
+    @Bean
+    VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        return new SimpleVectorStore(embeddingModel);
+    }
+}
